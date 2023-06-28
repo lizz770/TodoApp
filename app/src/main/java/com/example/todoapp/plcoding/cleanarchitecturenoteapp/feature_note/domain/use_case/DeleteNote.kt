b96d17c0da96678a96f929d05cb6546d4e6eb202
Elizaveta Maslichenko <maslicenkoelizaveta306@gmail.com>
2023-06-28 17:41:31 +1000

    удаления узла

---
+++ app/src/main/java/com/example/todoapp/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/DeleteNote.kt
@@ -0,0 +1,15 @@
+package com.example.todoapp.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.example.todoapp.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.example.todoapp.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+
+class DeleteNote {
+    class DeleteNote(
+        private val repository: NoteRepository
+    ) {
+
+        suspend operator fun invoke(note: Note) {
+            repository.deleteNote(note)
+        }
+    }
+}
